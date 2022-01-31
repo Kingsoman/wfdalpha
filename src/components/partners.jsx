7577dc7c8f3560f35601ef4a1dc858c932f70a4b
Hardin Santoso <havespolo@gmail.com>
2022-01-31 10:23:30 +0700

    Partners Fix

--- src/components/partners.jsx
+++ src/components/partners.jsx
@@ -1,9 +1,9 @@
-import { Box, SimpleGrid, Image, Text } from '@chakra-ui/react'
+import { Box, SimpleGrid, Image, Text,Link,Center } from '@chakra-ui/react'
 import * as React from 'react'
 
 export default function Partners() {
   return (
-    <Box as="section" p="10" mb={'28'} z-zIndex={'5'}>
+    <Box as="section" p="10" mb={'28'}>
       <Box
         maxW="7xl"
         mx="auto"
@@ -14,64 +14,86 @@ export default function Partners() {
         alignSelf={'center'}
         alignContent={'center'}
       >
-        <Text fontSize="36px" color={'white'}
-                style={{ fontFamily: 'PilatExtended-Bold' }} textAlign={'center'} mb={'25px'}>Our Partners</Text>
+        <Text 
+          fontSize="36px" color={'white'} 
+          style={{ fontFamily: 'PilatExtended-Bold' }} 
+          textAlign={'center'} mb={'35px'}>
+            Our Partners
+        </Text>
         <SimpleGrid
           ml={'50px'}
+          align='center'
           columns={{
             base: 1,
             md: 4,
           }}
           spacing="5"
         >
-          <a href='https://kommunitas.net/'
-          alignItems="center"
-          justifyContent="center"
-          alignContent='center'>
-            <Image src="/media/partners/Kommunitas.png" />
-          </a>
-          <a href='https://youtube.com/channel/UCmNM2yxDyy6NonRrzGSXQVA'
-          alignItems="center"
-          justifyContent="center"
-          alignContent='center'>
-            <Image src="/media/partners/pandai.png" />
-          </a>
-          <a href='https://linktr.ee/DanceroNFT'
-          alignItems="center"
-          justifyContent="center"
-          alignContent='center'>
-            <Image src="/media/partners/dancero.jpg" />
-          </a>
-          <a href='https://baksomania.com/'
-          alignItems="center"
-          justifyContent="center"
-          alignContent='center'>
-            <Image src="/media/partners/Baksomania.png" />
-          </a>
-          <a href='https://moggiesverse.com/'
-          alignItems="center"
-          justifyContent="center"
-          alignContent='center'>
-            <Image src="/media/partners/Moggie.jpg" />
-          </a>
-          <a href='https://pinecone.community/'
-          alignItems="center"
-          justifyContent="center"
-          alignContent='center'>
-            <Image src="/media/partners/pinecone.png" />
-          </a>
-          <a href='https://terraspaces.org/'
-          alignItems="center"
-          justifyContent="center"
-          alignContent='center'>
-            <Image src="/media/partners/terraspace.jpg" />
-          </a>
-          <a href='https://www.lunapad.co/'
-          alignItems="center"
-          justifyContent="center"
-          alignContent='center'>
-            <Image src="/media/partners/lunapad.png" />
-          </a>
+          <Link href='https://kommunitas.net/'
+          backgroundColor={'#f0f3fa'}
+          >
+            <Center
+            height={'100%'}
+            >
+              <Image src="/media/Kommunitas.png" />
+            </Center>
+          </Link>
+          <Link href='https://www.youtube.com/channel/UCmNM2yxDyy6NonRrzGSXQVA'
+          backgroundColor={'#f0f3fa'}>
+            <Center
+            height={'100%'}
+            >
+              <Image src="/media/pandai.png" />
+            </Center>
+          </Link>
+          <Link href='https://linktr.ee/DanceroNFT'
+          backgroundColor={'#f0f3fa'}>
+            <Center
+            height={'100%'}
+            >
+              <Image src="/media/dancero.jpg" />
+            </Center>
+          </Link>
+          <Link href='https://baksomania.com/'
+          backgroundColor={'#f0f3fa'}>
+            <Center
+            height={'100%'}
+            >
+              <Image src="/media/Baksomania.png" />
+            </Center>
+          </Link>
+          <Link href='https://moggiesverse.com/'
+          backgroundColor={'#f0f3fa'}>
+            <Center
+            height={'100%'}
+            >
+              <Image src="/media/Moggie.jpg" />
+            </Center>
+          </Link>
+          <Link href='https://www.pinecone.community/#/home'
+          backgroundColor={'#f0f3fa'}>
+            <Center
+            height={'100%'}
+            >
+              <Image src="/media/pinecone.png" />
+            </Center>
+          </Link>
+          <Link href='https://terraspaces.org/'
+          backgroundColor={'#f0f3fa'}>
+            <Center
+            height={'100%'}
+            >
+              <Image src="/media/terraspace.jpg" />
+            </Center>
+          </Link>
+          <Link href='https://www.lunapad.co/'
+          backgroundColor={'#f0f3fa'}>
+            <Center
+            height={'100%'}
+            >
+              <Image src="/media/lunapad.png" />
+            </Center>
+          </Link>
         </SimpleGrid>
       </Box>
     </Box>
